f43e472b0aaf4067c4edd638901e22bd658d6f60
Терещенко Владимир Дмитриевич <vd.tereshchenko@gbstudios.ru>
2016-04-19 17:53:36 +0300

    Update application types

--- app/watch/index.js
+++ app/watch/index.js
@@ -131,4 +131,4 @@ let pingIntervalId = setInterval(() => {
 pingIntervalId.unref();
 
 
-teonet.start('teo-node', '0.1.3', 3, 5, teoEventCb);
+teonet.start('teo-node,teo-watch', '0.1.4', 3, 5, teoEventCb);
